99e66a015ddc63ff9366389bd83974035ab27266
Mehran Hosseini <mehran.hosseini@ebcont.com>
2022-03-16 14:16:19 +0100

    init commit

---
+++ src/main/java/com/kingcode/liquibaseapp/LiquibaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.kingcode.liquibaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseAppApplication.class, args);
+    }
+
+}
